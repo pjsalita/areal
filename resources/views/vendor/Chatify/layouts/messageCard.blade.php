79c9764201657c6d5454efface6425ed8be6d17d
Pj Salita <pj.salita@gmail.com>
2022-04-29 21:53:21 +0800

    fix attachment preview

--- resources/views/vendor/Chatify/layouts/messageCard.blade.php
+++ resources/views/vendor/Chatify/layouts/messageCard.blade.php
@@ -15,7 +15,7 @@
     @if(@$attachment[2] == 'image')
     <div>
         <div class="message-card">
-            <div class="image-file chat-image" style="width: 250px; height: 150px;background-image: url('{{ asset('storage/'.config('chatify.attachments.folder').'/'.$attachment[0]) }}')">
+            <div class="image-file chat-image" style="width: 250px; height: 150px;background-image: url('{{ Storage::url(config('chatify.attachments.folder') . "/{$attachment[0]}") }}')">
             </div>
         </div>
     </div>
@@ -40,7 +40,7 @@
     @if(@$attachment[2] == 'image')
     <div>
         <div class="message-card mc-sender">
-            <div class="image-file chat-image" style="width: 250px; height: 150px;background-image: url('{{ asset('storage/'.config('chatify.attachments.folder').'/'.$attachment[0]) }}')">
+            <div class="image-file chat-image" style="width: 250px; height: 150px;background-image: url('{{ Storage::url(config('chatify.attachments.folder') . "/{$attachment[0]}") }}')">
             </div>
         </div>
     </div>

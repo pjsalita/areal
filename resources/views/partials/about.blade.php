d4af4c28de081373b19e73b03f3fefaa1cf8f3e6
Pj Salita <pj.salita@gmail.com>
2022-05-07 11:44:45 +0800

    how to

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -37,68 +37,25 @@
                 </div>
             </div>
             <div class="body">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, quam similique? Nesciunt debitis
-                pariatur officiis? Asperiores
-                a odit dolorum animi corporis, vero fugiat ad esse numquam labore sint totam repudiandae, similique
-                rerum tempora nesciunt
-                reiciendis laudantium deserunt iure assumenda magni error. Soluta nemo illo beatae vel, earum est
-                esse nostrum accusamus
-                quisquam adipisci aut minus repellat delectus, nesciunt officiis, perspiciatis minima possimus in.
-                Obcaecati nostrum,
-                similique sunt illo nulla quo consequatur quos possimus dolorum, consequuntur exercitationem eveniet
-                nesciunt commodi
-                temporibus aperiam, sapiente distinctio libero officiis tempora quaerat. Omnis ipsa fugiat facere
-                soluta est culpa, assumenda
-                consectetur laborum ea accusamus quidem rerum non illo molestiae mollitia adipisci laudantium atque
-                <!-- Developers : JC Hornales and JC Tapnio -->
-                Quidem asperiores minus laboriosam numquam eius nemo. Repellendus fugit ut fugiat? Voluptate
-                possimus rerum, debitis maiores
-                porro harum ea iure? Nostrum esse repudiandae aut dolor sed sunt corporis fugit beatae obcaecati
-                vero ex dolorum, atque
-                temporibus saepe error. Nemo nobis obcaecati adipisci, consequuntur voluptatem commodi ipsa debitis
-                culpa molestias eum
-                incidunt libero nostrum magni ut eius. Ipsa, tempora fugiat placeat velit vitae repudiandae facere
-                sint fuga dolorum fugit
-                doloremque illo sit ducimus nisi eveniet non quo delectus itaque amet maiores, sed repellendus
-                deserunt accusamus! Fugiat
-                facere eius vel illo iusto debitis reprehenderit molestias ducimus dicta ea fugit voluptatum quasi
-                beatae ipsam illum, at
-                quaerat numquam sequi corrupti labore, optio in commodi. Quia quasi facilis alias voluptas nulla
-                possimus velit pariatur quod
-                in unde, perferendis doloribus, est incidunt earum iure assumenda at! Exercitationem vitae
-                temporibus adipisci et voluptatum,
-                pariatur accusamus obcaecati suscipit voluptatem amet laborum esse quod? Veniam qui nulla nemo
-                beatae aliquid labore officiis
-                quisquam incidunt, ratione accusantium doloribus sed ad, animi possimus culpa rem fuga reprehenderit
-                facere! Debitis adipisci
-                quod, placeat explicabo repellendus unde reiciendis iste soluta deleniti ea alias ad nobis odit
-                cupiditate obcaecati aliquid
-                numquam? Iste nam rem nobis, beatae, alias ipsa enim, quaerat dolor aspernatur voluptas error.
-                Magni, consectetur accusantium!
-                Velit magnam sed fugit quia voluptatum. Quas aut sit corporis magni quidem optio. Perferendis,
-                voluptatem ratione animi magni
-                explicabo impedit doloribus atque eos dolore vero facilis deserunt accusamus corrupti aperiam
-                perspiciatis repellendus culpa
-                non adipisci consequatur. Aliquid est laudantium at voluptate error placeat unde. Eius optio
-                doloremque tempora alias nemo
-                corporis, labore tenetur nesciunt nobis dolores repellendus laborum inventore animi dicta aut porro
-                sint accusamus.
-                Voluptatibus, temporibus. Accusamus vero necessitatibus officiis iste voluptate magni deserunt, at,
-                perferendis quod nisi
-                voluptas dolor nobis architecto facilis nam, exercitationem incidunt adipisci obcaecati dignissimos
-                molestias! Sunt beatae
-                blanditiis exercitationem eum quae ducimus id ratione adipisci nesciunt optio commodi fugit culpa
-                tempora provident
-                praesentium natus expedita velit quasi quam tenetur neque earum, enim explicabo quaerat! Quia illo
-                magnam architecto
-                repellendus, voluptate odit labore quaerat sint reiciendis inventore similique eaque dolor sit nihil
-                accusantium dignissimos
-                nobis? Perferendis sed voluptates iste corporis eos accusamus, nesciunt architecto sequi ut officia
-                similique dolorum expedita
-                porro odit, explicabo obcaecati facere veritatis vitae, provident dolores vero? Voluptate animi eum
-                itaque dignissimos beatae
-                doloribus, delectus mollitia magni laboriosam eos consequatur autem fuga libero magnam quibusdam
-                deserunt quod!
+                <ol>
+			<li>
+				<p>Scroll down to About Areal App and press "download now".</p>
+				<img src="{{ asset('assets/images/1.png') }}" alt="" />
+			</li>
+			<li>
+				<p>Press details to go to the browser download tab.</p>
+				<img src="{{ asset('assets/images/2.png') }}" alt="" />
+			</li>
+			<li>
+				<p>Press the downloaded apk file. Then click the install.</p>
+				<img src="{{ asset('assets/images/3.png') }}" alt="" />
+				<img src="{{ asset('assets/images/4.png') }}" alt="" />
+			</li>
+			<li>
+				<p>Then open the application. </p>
+				<img src="{{ asset('assets/images/3.png') }}" alt="" />
+			</li>
+		</ol>
             </div>
         </div>
     </div>
